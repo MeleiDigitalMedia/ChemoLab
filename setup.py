be3440165d0defb0dfcb28b3b94a2d4d100255ad
MeleiDigitalMedia <59949918+MeleiDigitalMedia@users.noreply.github.com>
2020-02-10 00:16:07 +0530

    modified

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ setup(
     name='chemolab',
     description='ChemoLab is an educational based chemistry library for Python',
     packages=['chemolab', 'chemolab/core', 'chemolab/elements', 'chemolab/elements/data'],
-    version='1.0.0',
+    version='1.0.0-beta_001',
     author='MeleiDigitalMedia',
     author_email='meleidigitalmedia@gmail.com',
     url='https://github.com/MeleiDigitalMedia/ChemoLab/',
-    download_url='https://github.com/MeleiDigitalMedia/ChemoLab/archive/1.0.0-beta.tar.gz',
+    download_url='https://github.com/MeleiDigitalMedia/ChemoLab/archive/1.0.0-beta_001.tar.gz',
     license='MIT',
     python_requires='>=3.8',
     include_package_data=True
